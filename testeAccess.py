81b93628d6e3d609b951b2537d1315a92dcdc3e2
WhySoFast <matheusvca@gmail.com>
2020-12-31 17:24:31 -0300

    Listing table fix and References improvements

--- testeAccess.py
+++ testeAccess.py
@@ -10,6 +10,9 @@ from PyQt5.QtCore import Qt
 
 import pyodbc 
 
+# COMPILADOR EXECUTÁVEL
+# python -m PyInstaller testeAccess.py --onefile
+
 class mainWindow(QDialog):
 
     def __init__(self):
@@ -121,6 +124,9 @@ class CorrectionTab(QWidget):
             self.pathMDB_LineEdit.setText(pathMDB)
 
         def load_csv(self):
+
+            self.ColTrafo_ComboBox.clear()
+            self.ColKW_ComboBox.clear()
 
             pathCSV = QFileDialog.getOpenFileName(self, 'Open CSV/XLSX file',
                                                 str(pathlib.Path.home()), "CSV files (*.csv;*.xlsx)")
@@ -252,47 +258,63 @@ class FindReferenceTab(QWidget):
         self.InitUI()
 
     def InitUI(self):
-        self.GroupBox_Layout = QVBoxLayout()
+        self.GroupBox_Layout = QGridLayout()
         self.GroupBox_Layout.setAlignment(Qt.AlignCenter)
 
-        self.UploadMDB_Button = QPushButton("Escolha uma base MDB ")
+
+        self.MDBGroupBox = QGroupBox()
+        self.MDBGroupBox_Layout = QHBoxLayout()
+        self.UploadMDB_Button = QPushButton("Escolha uma base MDB")
         self.UploadMDB_Button.clicked.connect(self.loadMDB)
         self.UploadMDB_Button.setFixedWidth(150)
         
         self.pathMDB_LineEdit = QLineEdit("")
         self.pathMDB_LineEdit.setReadOnly(True)
         self.pathMDB_LineEdit.setFixedWidth(150)
+        self.MDBGroupBox_Layout.addWidget(self.UploadMDB_Button)
+        self.MDBGroupBox_Layout.addWidget(self.pathMDB_LineEdit)
+        self.MDBGroupBox.setLayout(self.MDBGroupBox_Layout)
 
 
-        self.RefGroupBox = QGroupBox("Referência")
 
-        self.RefGroupBox_Layout = QHBoxLayout()
-        #self.RefGroupBox_Layout.setAlignment(Qt.AlignCenter)
-
+        self.RefGroupBox = QGroupBox()
+        self.RefGroupBox_Layout = QGridLayout()
+        self.RefGroupBox_Layout.setAlignment(Qt.AlignCenter)
+        self.Reference_Label = QLabel("Referência:")
         self.Reference_LineEdit = QLineEdit("")
         self.Reference_LineEdit.setFixedWidth(150)
-
-
-
         self.FoundCircuit_Label = QLabel("Alimentador:")
         self.FoundCircuit_LineEdit = QLineEdit("")
         self.FoundCircuit_LineEdit.setReadOnly(True)
         self.FoundCircuit_LineEdit.setFixedWidth(150)
+        self.ReferenceEquipament_Label = QLabel("Equipamento:")
+        self.ReferenceEquipament_LineEdit = QLineEdit("")
+        self.ReferenceEquipament_LineEdit.setReadOnly(True)
+        self.ReferenceEquipament_LineEdit.setFixedWidth(150)
+        self.ReferenceStatus_Label = QLabel("Estado Normal/Atual:")
+        self.ReferenceStatus_LineEdit = QLineEdit("")
+        self.ReferenceStatus_LineEdit.setReadOnly(True)
+        self.ReferenceStatus_LineEdit.setFixedWidth(150)
 
-        self.RefGroupBox_Layout.addWidget(self.Reference_LineEdit)
-        self.RefGroupBox_Layout.addWidget(self.FoundCircuit_Label)
-        self.RefGroupBox_Layout.addWidget(self.FoundCircuit_LineEdit)
+        self.RefGroupBox_Layout.addWidget(self.Reference_Label,0,0)
+        self.RefGroupBox_Layout.addWidget(self.Reference_LineEdit,0,1)
+        self.RefGroupBox_Layout.addWidget(self.FoundCircuit_Label,0,2)
+        self.RefGroupBox_Layout.addWidget(self.FoundCircuit_LineEdit,0,3)
+        self.RefGroupBox_Layout.addWidget(self.ReferenceEquipament_Label,1,2)
+        self.RefGroupBox_Layout.addWidget(self.ReferenceEquipament_LineEdit,1,3)
+        self.RefGroupBox_Layout.addWidget(self.ReferenceStatus_Label,2,2)
+        self.RefGroupBox_Layout.addWidget(self.ReferenceStatus_LineEdit,2,3)
         self.RefGroupBox.setLayout(self.RefGroupBox_Layout)
 
         self.Find_Button = QPushButton("Procurar")
         self.Find_Button.clicked.connect(self.FindReference)
         self.Find_Button.setFixedWidth(150)
 
-        self.GroupBox_Layout.addWidget(self.UploadMDB_Button)
-        self.GroupBox_Layout.addWidget(self.pathMDB_LineEdit)
-        self.GroupBox_Layout.addWidget(self.RefGroupBox)
-        self.GroupBox_Layout.addWidget(self.Find_Button)
+        self.GroupBox_Layout.addWidget(self.MDBGroupBox,2,0)
+        self.GroupBox_Layout.addWidget(self.RefGroupBox,3,0)
+        self.GroupBox_Layout.addWidget(self.Find_Button,4,0)
 
+        self.GroupBox_Layout.setAlignment(Qt.AlignCenter)
         self.setLayout(self.GroupBox_Layout)
 
     
@@ -323,39 +345,53 @@ class FindReferenceTab(QWidget):
 
         reference = self.Reference_LineEdit.text().split(' ')[0].split('\n')[0].upper()
         
+        self.FoundCircuit_LineEdit.clear()
+        self.ReferenceEquipament_LineEdit.clear()
+        self.ReferenceStatus_LineEdit.clear()
 
         # print(reference)
-        
-        cursor.execute('select CIRC_ID from CARGA where CODIGO=?',reference)
-        circuit_id = cursor.fetchone()
-        # print(circuit_id)
-        
-        if circuit_id:
-            circuit_id = str(circuit_id).split('(')[1].split(',')[0]
+        refTypes = ['CARGA', 'BARRA','CHAVE','REGULADOR','REATOR','RAIOX','CAPACITOR']
 
-        else:
-            cursor.execute('select CIRC_ID from BARRA where CODIGO=?',reference)
-            circuit_id = cursor.fetchone()
-            if circuit_id:
+        for ref in refTypes:
+            cursor.execute('select CIRC_ID from ' + ref + ' where CODIGO=?',reference)
+            circuit_id = cursor.fetchone() 
+            if circuit_id :
                 circuit_id = str(circuit_id).split('(')[1].split(',')[0]
-            # print(circuit_id)
-
+                equipament = ref
+                break
 
         cursor.execute('select CODIGO from CIRCUITO where CIRC_ID=?', circuit_id)
 
         circuit_name = cursor.fetchone()
 
-        if circuit_name:
+        if circuit_id and int(circuit_id)>0:
             circuit_name = str(circuit_name).split('(')[1].split(',')[0].split('\'')[1]
             # print(circuit_name)
             self.FoundCircuit_LineEdit.setText(circuit_name)
             self.FoundCircuit_LineEdit.setStyleSheet("color : blue")
+            self.ReferenceEquipament_LineEdit.setText(equipament)
+            if equipament== "CHAVE":
+                cursor.execute('select ESTADO from CHAVE where CODIGO=?',reference)
+                if str(cursor.fetchone()).split('(')[1].split(',')[0] == '1':
+                    currentState = 'Fechado'
+                else:
+                    currentState = 'Aberto'
+
+                cursor.execute('select ESTADO_NORMAL from CHAVE where CODIGO=?',reference)
+                if str(cursor.fetchone()).split('(')[1].split(',')[0] == '1':
+                    normalState = 'Fechado'
+                else:
+                    normalState = 'Aberto'
+
+                self.ReferenceStatus_LineEdit.setText(normalState + '/' + currentState)
+                
         else:
             self.FoundCircuit_LineEdit.setText("Not Found")
             self.FoundCircuit_LineEdit.setStyleSheet("color : red")
 
 if __name__ == '__main__':
     APP = QApplication(sys.argv)
+    APP.setStyle('Fusion')
 
     GUI = mainWindow()
     GUI.show()
